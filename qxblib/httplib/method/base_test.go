a2e0d3721d0f5da40c1ad5833dd032a7c2b293cb
钱晓波 <57698932@qq.com>
2022-02-10 11:40:15 +0800

    1111

---
+++ qxblib/httplib/method/base_test.go
@@ -0,0 +1,14 @@
+package method
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	fmt.Println(Get("https://www.jianshu.com/p/937eff8e48c7"))
+}
+
+func TestPost(t *testing.T) {
+
+}
